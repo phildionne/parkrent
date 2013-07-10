d859151c8417d151f0498db1b406f2e579076c68
Philippe Dionne <dionne.phil@gmail.com>
2013-07-10 09:35:51 -0400

    Add Rent factory

---
+++ spec/factories/rent.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :rent do
+    price       { [50, 100].sample }
+    beginning   { Date.today }
+    termination { Date.today + 1.day }
+  end
+
+  factory :invalid_rent, parent: :rent do
+    price       nil
+    beginning   nil
+    termination nil
+  end
+end
