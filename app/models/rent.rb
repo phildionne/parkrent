bc61055dc9ebd57dff6c03c14072df1a4ba262fa
Philippe Dionne <dionne.phil@gmail.com>
2013-07-10 09:36:27 -0400

    Add Rent validations

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -1,2 +1,4 @@
 class Rent < ActiveRecord::Base
+  validates :price, :beginning, :termination, presence: true
+  validates :price, numericality: true
 end
---
+++ spec/models/rent_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe Rent do
+
+  describe :Factories do
+    context "Valid factory" do
+      subject { FactoryGirl.create(:rent) }
+      specify { should be_valid }
+    end
+
+    context "Invalid factory" do
+      subject { FactoryGirl.build(:invalid_rent) }
+      specify { should_not be_valid }
+    end
+  end
+
+  describe :Associations do
+  end
+
+  describe :Validations do
+    it { should validate_presence_of(:price) }
+    it { should validate_presence_of(:beginning) }
+    it { should validate_presence_of(:termination) }
+    it { should validate_numericality_of(:price) }
+  end
+
+  describe :Callbacks do
+  end
+
+  describe :ClassMethods do
+  end
+
+  describe :InstanceMethods do
+  end
+
+end
