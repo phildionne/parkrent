bf51ff5fdcc573b8d806cd52911c02befe453fff
Philippe Dionne <dionne.phil@gmail.com>
2013-07-10 17:00:52 -0400

    rails g resource Transaction

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ParkRent::Application.routes.draw do
   resources :parkings
+  resources :transactions
 
   resources :rents
   root :to => 'frontpage#show'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130710144623) do
+ActiveRecord::Schema.define(version: 20130710205926) do
 
   create_table "parkings", force: true do |t|
     t.string   "location"
@@ -26,6 +26,11 @@ ActiveRecord::Schema.define(version: 20130710144623) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "parking_id"
+  end
+
+  create_table "transactions", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
