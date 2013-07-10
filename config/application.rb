b774e5831e2653e498e91c516ccf38aa649aeff9
Philippe Dionne <dionne.phil@gmail.com>
2013-07-10 13:01:35 -0400

    Recursively load files in the /models directory

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,9 @@ module ParkRent
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # Recursively load models in subdirectories
+    config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**}')]
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
