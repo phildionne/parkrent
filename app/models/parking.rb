6a6d8ef9c8aff600c11e657d139522fd82c78caa
Philippe Dionne <dionne.phil@gmail.com>
2013-07-11 16:38:17 -0400

    Accept rent attributes on Parking

--- app/models/parking.rb
+++ app/models/parking.rb
@@ -1,6 +1,8 @@
 class Parking < ActiveRecord::Base
   has_many :rents, dependent: :destroy
 
+  accepts_nested_attributes_for :rents
+
   validates :location, presence: true
   validates_with RentValidator
 end
