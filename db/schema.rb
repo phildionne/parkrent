c1d60f12e7f8699589020f9219b3d39031f5fb40
Philippe Dionne <dionne.phil@gmail.com>
2013-07-10 21:07:58 -0400

    rails g resource order

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130710205926) do
+ActiveRecord::Schema.define(version: 20130711005151) do
+
+  create_table "orders", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "parkings", force: true do |t|
     t.string   "location"
