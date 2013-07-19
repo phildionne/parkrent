70a7718bbeaa79970cf63f94c53bde631745cdb8
Philippe Dionne <dionne.phil@gmail.com>
2013-07-18 22:05:41 -0400

    Add #full_name to Contactable

--- app/models/concerns/contactable.rb
+++ app/models/concerns/contactable.rb
@@ -6,4 +6,8 @@ module Contactable
   included do
     validates :first_name, :last_name, :phone_number, :email, presence: true
   end
+
+  def full_name
+    [first_name, last_name].join(' ')
+  end
 end
