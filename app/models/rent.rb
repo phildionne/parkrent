17a8c2b5ffc4df39784710aefc2167591b0ea6f2
Philippe Dionne <dionne.phil@gmail.com>
2013-08-07 22:26:31 -0400

    Add Rent#title

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -4,4 +4,8 @@ class Rent < ActiveRecord::Base
   validates :parking, :price, :beginning, :termination, presence: true
   validates :price, numericality: { greater_than: 0 }
   validates_with RentValidator
+
+  def title
+    "#{price} - #{beginning} #{termination}"
+  end
 end
