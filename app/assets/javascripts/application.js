47d9c89fcfa8c779641a10925de1009e0179dcb2
Philippe Dionne <dionne.phil@gmail.com>
2013-08-11 16:26:22 -0400

    Add jquery.geocomplete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery.geocomplete.min
 //= require cocoon
 //= require bootstrap-datepicker/bootstrap-datepicker
 //= require jquery_ujs
