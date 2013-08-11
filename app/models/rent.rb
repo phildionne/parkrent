571bd6a0287fa7ebc3f8f32f22908b138840e689
Philippe Dionne <dionne.phil@gmail.com>
2013-08-11 11:50:38 -0400

    Validate Rent #beginning is after current time

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -4,6 +4,7 @@ class Rent < ActiveRecord::Base
   validates :parking, :price, :beginning, :termination, presence: true
   validates :price, numericality: { greater_than: 0 }
   validates :beginning, :date => { :before => :termination }
+  validates :beginning, :date => { :after => Proc.new { Time.now }}
   validates_with RentValidator
 
   def title
