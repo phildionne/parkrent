ec4dc7a49afeee7e9848a222bfcb84f244f8aa47
Philippe Dionne <dionne.phil@gmail.com>
2013-08-14 20:22:04 -0400

    Fix Rent factory beginning and termination values

--- spec/factories/rent.rb
+++ spec/factories/rent.rb
@@ -3,8 +3,8 @@ FactoryGirl.define do
     parking
 
     price       { [50, 100].sample }
-    beginning   { Date.today }
-    termination { Date.today + 1.day }
+    beginning   { Date.tomorrow }
+    termination { beginning + [1, 40, 120].sample.day }
   end
 
   factory :invalid_rent, parent: :rent do
