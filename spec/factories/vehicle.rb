856d58ea24a5aadd94b8dbee1c3a81c817731d99
Philippe Dionne <dionne.phil@gmail.com>
2013-09-04 18:38:44 -0400

    Use valid license plates in Vehicle factory

--- spec/factories/vehicle.rb
+++ spec/factories/vehicle.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :vehicle do
     user
 
-    license_plate { [Faker::Lorem.characters(3), Faker::Number.number(3)].flatten.shuffle.join }
+    license_plate { ['A00 ABC', 'ABC 000', '000H000', 'CC 000', 'CD 000'].sample }
   end
 
   factory :invalid_vehicle, parent: :vehicle do
