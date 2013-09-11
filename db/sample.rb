ee11f2d955d1d0e12647a391f9382a63f0ef162a
Philippe Dionne <dionne.phil@gmail.com>
2013-09-11 18:31:06 -0400

    Add users with orders to sample

--- db/sample.rb
+++ db/sample.rb
@@ -1,1 +1,6 @@
 FactoryGirl.create_list(:user_with_parkings_and_rents, 10)
+
+User.limit(5).each do |user|
+  FactoryGirl.create(:vehicle, user: user)
+  FactoryGirl.create_list(:order_with_payment, 3, user: user)
+end
