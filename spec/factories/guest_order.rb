b6d716d6a1e3356aed73f6e2ce571419fe271fcd
Philippe Dionne <dionne.phil@gmail.com>
2013-09-19 21:21:59 -0400

    Add year and model attributes to Vehicle factory

--- spec/factories/guest_order.rb
+++ spec/factories/guest_order.rb
@@ -10,6 +10,8 @@ FactoryGirl.define do
     password_confirmation { password }
 
     license_plate { ['A00 ABC', 'ABC 000', '000H000', 'CC 000', 'CD 000'].sample }
+    year          { (1995..2013).to_a.sample }
+    model         { ['Black Maybach', 'Range Rover', 'Audi A8', 'Volvo'].sample }
   end
 
   factory :invalid_guest_order, parent: :guest_order do
--- spec/factories/vehicle.rb
+++ spec/factories/vehicle.rb
@@ -3,6 +3,8 @@ FactoryGirl.define do
     user
 
     license_plate { ['A00 ABC', 'ABC 000', '000H000', 'CC 000', 'CD 000'].sample }
+    year          { (1995..2013).to_a.sample }
+    model         { ['Black Maybach', 'Range Rover', 'Audi A8', 'Volvo'].sample }
   end
 
   factory :invalid_vehicle, parent: :vehicle do
