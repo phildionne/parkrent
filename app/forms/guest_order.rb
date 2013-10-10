22ede5f9c5d1bfa1120d0f4983cc83a4b784b772
Philippe Dionne <dionne.phil@gmail.com>
2013-10-10 18:06:22 -0400

    Fix form objects Associates syntax

--- app/forms/guest_order.rb
+++ app/forms/guest_order.rb
@@ -1,8 +1,10 @@
 class GuestOrder
   include Associates
 
-  associate :first_name, :last_name, :phone_number, :email,
-            :password, :password_confirmation, :confirmation_token, to: :user
-  associate :license_plate, :model, :year, to: :vehicle, depend_on: :user
-  associate :price, :rent, :rent_id,       to: :order,   depend_on: [:user, :vehicle]
+  associate :user, only: [:first_name, :last_name, :phone_number, :email,
+            :password, :password_confirmation, :confirmation_token]
+  associate :vehicle, depends_on: :user
+  associate :rent
+
+  associate :order, depend_on: [:user, :vehicle, :rent]
 end
--- app/forms/onboarding.rb
+++ app/forms/onboarding.rb
@@ -1,10 +1,10 @@
 class Onboarding
   include Associates
 
-  associate :first_name, :last_name, :phone_number, :email,
-            :password, :password_confirmation, :confirmation_token, to: :user
-  associate :location, to: :parking, depend_on: :user
-  associate :beginning, :termination, :price, to: :rent, depend_on: :parking
+  associate :user, only: [:first_name, :last_name, :phone_number, :email,
+            :password, :password_confirmation, :confirmation_token]
+  associate :parking,                                        depends_on: :user
+  associate :rent, only: [:beginning, :termination, :price], depends_on: :parking
 
   # @return [Boolean] Wether the Onboarding is only missing the user's password & password_confirmation
   def can_assign_credential?
