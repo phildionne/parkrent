bf87f8d4cb00208fae60e3cbf8170bc19be526bf
Philippe Dionne <dionne.phil@gmail.com>
2013-10-14 16:24:30 -0400

    Overhaul GuestOrdersController#create to work with devise confirmable

--- app/controllers/guest_orders_controller.rb
+++ app/controllers/guest_orders_controller.rb
@@ -12,10 +12,16 @@ class GuestOrdersController < ApplicationController
     @guest_order = GuestOrder.new(permitted_params)
 
     if @guest_order.save
-      sign_in @guest_order.user
+      if @guest_order.user.active_for_authentication?
+        sign_in @guest_order.user
 
-      redirect_to @guest_order.order, notice: 'Order was successfully created.'
+        redirect_to @guest_order.order, notice: I18n.t('guest_orders.order_created_and_signed_up')
+      else
+        # @FIXME Same problem than OnboardingsController#create
+        redirect_to root_path, notice: I18n.t('guest_orders.order_created_and_signed_up_but_unconfirmed')
+      end
     else
+      # @TODO Same problem than OnboardingsController#create
       render action: :new
     end
   end
