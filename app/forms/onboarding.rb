44ce6758a83d645e22fcb2e5013920f5b944971c
Philippe Dionne <dionne.phil@gmail.com>
2013-10-14 12:48:10 -0400

    Update Onboarding password_form logic

--- app/forms/onboarding.rb
+++ app/forms/onboarding.rb
@@ -9,6 +9,6 @@ class Onboarding
   # @return [Boolean] Wether the Onboarding is only missing the user's password & password_confirmation
   def can_assign_credential?
     valid?
-    errors.keys == [:password, :password_confirmation]
+    errors.keys == [:password, :password_confirmation] || [:password] || [:password_confirmation]
   end
 end
