2a3b9f850fcaba8e572ee888d22b3943602134a3
Philippe Dionne <dionne.phil@gmail.com>
2013-10-15 20:01:02 -0400

    Add rent doesn't belong to user validation on OrderValidator and refactor

--- app/models/validators/order_validator.rb
+++ app/models/validators/order_validator.rb
@@ -3,14 +3,25 @@ class OrderValidator < ActiveModel::Validator
   # @param record [Order]
   def validate(record)
     validate_vehicle_belongs_to_user(record)
+    validate_rent_doesnt_belongs_to_user(record)
   end
 
 
   private
 
+  def validate_rent_doesnt_belongs_to_user(record)
+    if record.user && record.rent
+      if record.user == record.rent.user
+        record.errors[:rent] = "can't belong to the user"
+      end
+    end
+  end
+
   def validate_vehicle_belongs_to_user(record)
-    unless record.user && record.vehicle && record.user == record.vehicle.try(:user)
-      record.errors[:vehicle] = "must belong to the user."
+    if record.user && record.vehicle
+      unless record.user == record.vehicle.user
+        record.errors[:vehicle] = "must belong to the user"
+      end
     end
   end
 end
--- spec/models/validators/order_validator_spec.rb
+++ spec/models/validators/order_validator_spec.rb
@@ -2,13 +2,24 @@ require 'spec_helper'
 
 describe OrderValidator do
 
-  let(:order) { build(:order, vehicle: nil) }
-  let(:user) { create(:user_with_vehicles) }
+  describe :validate_rent_doesnt_belongs_to_user do
+    let(:user)  { create(:user_with_parkings_and_rents) }
+    let(:order) { build(:order, user: user, rent: user.rents.sample) }
 
-  it "validates vehicle belongs to user" do
-    order.vehicle = user.vehicles.sample
-    order.save
+    it "validates rent doesn't belongs to user" do
+      order.valid?
+      expect(order).to have(1).error_on(:rent)
+    end
+  end
 
-    expect(order).to have(1).error_on(:vehicle)
+  describe :validate_vehicle_belongs_to_user do
+    let(:user)    { create(:user) }
+    let(:vehicle) { create(:vehicle) }
+    let(:order)   { build(:order, user: user, vehicle:vehicle) }
+
+    it "validates vehicle belongs to user" do
+      order.valid?
+      expect(order).to have(1).error_on(:vehicle)
+    end
   end
 end
