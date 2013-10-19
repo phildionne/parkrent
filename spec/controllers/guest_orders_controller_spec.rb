0abc553a3580429d78337783ed7c3d4e3c598d41
Philippe Dionne <dionne.phil@gmail.com>
2013-10-19 17:20:50 -0400

    Major improvements to controllers specs

--- spec/controllers/guest_orders_controller_spec.rb
+++ spec/controllers/guest_orders_controller_spec.rb
@@ -1,8 +1,33 @@
 require 'spec_helper'
 
 describe GuestOrdersController do
-  describe "GET new" do
-    context "with valid params" do
+
+  context "with an authenticated user" do
+    let(:user) { create(:user) }
+    before do
+      user.confirm!
+      sign_in user
+    end
+
+    describe "GET new" do
+
+      it "redirects to root path" do
+        get :new
+        expect(response).to be_redirect
+      end
+    end
+
+    describe "POST create" do
+
+      it "redirects to root path" do
+        post :create, { guest_order: {} }
+        expect(response).to be_redirect
+      end
+    end
+  end
+
+  context "with an unauthenticated user" do
+    describe "GET new" do
       let(:rent) { create(:rent) }
 
       before { get :new, { rent_id: rent.id } }
@@ -18,54 +43,50 @@ describe GuestOrdersController do
       end
     end
 
-    context "with invalid params" do
-      pending
-    end
-  end
+    describe "POST create" do
+      let(:rent) { create(:rent) }
 
-  describe "POST create" do
-    let(:rent) { create(:rent) }
+      context "with valid params" do
+        let(:guest_order_attributes) { attributes_for(:guest_order, rent_id: rent.id) }
 
-    context "with valid params" do
-      let(:guest_order_attributes) { attributes_for(:guest_order, rent_id: rent.id) }
+        it "creates a new GuestOrder" do
+          expect {
+            post :create, { guest_order: guest_order_attributes }
+          }.to change(Order, :count).by(1) && change(User, :count).by(1) && change(Vehicle, :count).by(1)
+        end
 
-      it "creates a new GuestOrder" do
-        expect {
+        it "assigns a newly created guest_order as @guest_order" do
           post :create, { guest_order: guest_order_attributes }
-        }.to change(Order, :count).by(1) && change(User, :count).by(1) && change(Vehicle, :count).by(1)
-      end
+          expect(assigns(:guest_order)).to         be_a(GuestOrder)
+          expect(assigns(:guest_order).order).to   be_persisted
+          expect(assigns(:guest_order).user).to    be_persisted
+          expect(assigns(:guest_order).vehicle).to be_persisted
+        end
 
-      it "assigns a newly created guest_order as @guest_order" do
-        post :create, { guest_order: guest_order_attributes }
-        expect(assigns(:guest_order)).to         be_a(GuestOrder)
-        expect(assigns(:guest_order).order).to   be_persisted
-        expect(assigns(:guest_order).user).to    be_persisted
-        expect(assigns(:guest_order).vehicle).to be_persisted
-      end
-
-      it "assigns the correct rent" do
-        post :create, { guest_order: guest_order_attributes }
-        expect(assigns(:guest_order).rent_id).to eq(rent.id)
-      end
-
-      context "when unconfirmed" do
-
-        it "redirects to the root path" do
+        it "assigns the correct rent" do
           post :create, { guest_order: guest_order_attributes }
-          expect(response).to redirect_to(root_path)
+          expect(assigns(:guest_order).rent_id).to eq(rent.id)
+        end
+
+        context "when unconfirmed" do
+
+          it "redirects to the root path" do
+            post :create, { guest_order: guest_order_attributes }
+            expect(response).to redirect_to(root_path)
+          end
         end
       end
-    end
 
-    context "with invalid params" do
-      before { post :create, guest_order: attributes_for(:invalid_guest_order, rent_id: rent.id) }
+      context "with invalid params" do
+        before { post :create, guest_order: attributes_for(:invalid_guest_order, rent_id: rent.id) }
 
-      it "assigns a newly created but unsaved guest_order as @guest_order" do
-        expect(assigns(:guest_order)).to be_a(GuestOrder)
-        expect(assigns(:guest_order)).not_to be_persisted
+        it "assigns a newly created but unsaved guest_order as @guest_order" do
+          expect(assigns(:guest_order)).to be_a(GuestOrder)
+          expect(assigns(:guest_order)).not_to be_persisted
+        end
+
+        it { should render_template :new }
       end
-
-      it { should render_template :new }
     end
   end
 end
--- spec/controllers/onboardings_controller_spec.rb
+++ spec/controllers/onboardings_controller_spec.rb
@@ -1,56 +1,83 @@
 require 'spec_helper'
 
 describe OnboardingsController do
-  describe "GET new" do
-    before { get :new }
 
-    it "assigns a new onboarding as @onboarding" do
-      expect(assigns(:onboarding)).to be_a(Onboarding)
-      expect(assigns(:onboarding)).not_to be_persisted
+  context "with an authenticated user" do
+    let(:user) { create(:user) }
+    before do
+      user.confirm!
+      sign_in user
     end
 
-    it "responds with success and render template" do
-      expect(response).to be_success
-      expect(response).to render_template :new
+    describe "GET new" do
+
+      it "redirects to root path" do
+        get :new
+        expect(response).to be_redirect
+      end
+    end
+
+    describe "POST create" do
+
+      it "redirects to root path" do
+        post :create, { onboarding: {} }
+        expect(response).to be_redirect
+      end
     end
   end
 
-  describe "POST create" do
-    context "with valid params" do
-      let(:onboarding_attributes) { attributes_for(:onboarding) }
+  context "with an unauthenticated user" do
+    describe "GET new" do
+      before { get :new }
 
-      it "creates a new Onboarding" do
-        expect {
-          post :create, { onboarding: onboarding_attributes }
-        }.to change(User, :count).by(1) && change(Parking, :count).by(1) && change(Rent, :count).by(1)
-      end
-
-      it "assigns a newly created onboarding as @onboarding" do
-        post :create, { onboarding: onboarding_attributes }
-        expect(assigns(:onboarding)).to         be_a(Onboarding)
-        expect(assigns(:onboarding).user).to    be_persisted
-        expect(assigns(:onboarding).parking).to be_persisted
-        expect(assigns(:onboarding).rent).to    be_persisted
-      end
-
-      context "when unconfirmed" do
-
-        it "redirects to the root path" do
-          post :create, { onboarding: onboarding_attributes }
-          expect(response).to redirect_to(root_path)
-        end
-      end
-    end
-
-    context "with invalid params" do
-      before { post :create, onboarding: attributes_for(:invalid_onboarding) }
-
-      it "assigns a newly created but unsaved onboarding as @onboarding" do
+      it "assigns a new onboarding as @onboarding" do
         expect(assigns(:onboarding)).to be_a(Onboarding)
         expect(assigns(:onboarding)).not_to be_persisted
       end
 
-      it { should render_template :new }
+      it "responds with success and render template" do
+        expect(response).to be_success
+        expect(response).to render_template :new
+      end
+    end
+
+    describe "POST create" do
+      context "with valid params" do
+        let(:onboarding_attributes) { attributes_for(:onboarding) }
+
+        it "creates a new Onboarding" do
+          expect {
+            post :create, { onboarding: onboarding_attributes }
+          }.to change(User, :count).by(1) && change(Parking, :count).by(1) && change(Rent, :count).by(1)
+        end
+
+        it "assigns a newly created onboarding as @onboarding" do
+          post :create, { onboarding: onboarding_attributes }
+          expect(assigns(:onboarding)).to         be_a(Onboarding)
+          expect(assigns(:onboarding).user).to    be_persisted
+          expect(assigns(:onboarding).parking).to be_persisted
+          expect(assigns(:onboarding).rent).to    be_persisted
+        end
+
+        context "when unconfirmed" do
+
+          it "redirects to the root path" do
+            post :create, { onboarding: onboarding_attributes }
+            expect(response).to redirect_to(root_path)
+          end
+        end
+      end
+
+      context "with invalid params" do
+        before { post :create, onboarding: attributes_for(:invalid_onboarding) }
+
+        it "assigns a newly created but unsaved onboarding as @onboarding" do
+          expect(assigns(:onboarding)).to be_a(Onboarding)
+          expect(assigns(:onboarding)).not_to be_persisted
+        end
+
+        it { should render_template :new }
+      end
     end
   end
 end
