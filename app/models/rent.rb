4152d25885cb525cd3af0bc68994202dff4fbfa2
Philippe Dionne <dionne.phil@gmail.com>
2013-11-03 13:50:45 -0500

    Improve Rent schedule attribute setter/getter

--- app/models/rent.rb
+++ app/models/rent.rb
@@ -1,12 +1,14 @@
 class Rent < ActiveRecord::Base
   include Authority::Abilities
 
+  serialize :schedule, Hash
+
   belongs_to :parking
   has_one :user, through: :parking
   has_many :orders
 
   monetize :price_cents, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10000 }
-  validates :parking, :price, :beginning, :termination, presence: true
+  validates :parking, :price, :beginning, :termination, :schedule, presence: true
   validates :beginning, date: { before: :termination }
   validates :beginning, date: { after: Proc.new { Date.yesterday.at_end_of_day }}
   validates_with RentValidator
@@ -15,11 +17,17 @@ class Rent < ActiveRecord::Base
     "#{price.format} - #{beginning.to_formatted_s(:short)} #{termination.to_formatted_s(:short)}"
   end
 
+  # @return [IceCube::Schedule]
   def schedule
-    IceCube::Schedule.from_yaml(read_attribute(:schedule)) if read_attribute(:schedule)
+    IceCube::Schedule.from_hash(read_attribute(:schedule))
   end
 
-  def schedule=(schedule)
-    write_attribute(:schedule, schedule.to_yaml)
+  # @param new_schedule [IceCube::Schedule]
+  # @return [IceCube::Schedule]
+  def schedule=(new_schedule)
+    schedule_will_change! unless new_schedule == schedule
+    write_attribute(:schedule, new_schedule.to_hash)
+  end
+
   end
 end
