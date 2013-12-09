d24346fb298b5f7cc4c526ca4be1cb89d8274f8c
Philippe Dionne <dionne.phil@gmail.com>
2013-12-09 14:43:38 -0500

    Configure default application locale

--- config/application.rb
+++ config/application.rb
@@ -26,8 +26,12 @@ module ParkRent
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
 
+    config.i18n.available_locales = [:en, :fr]
+    config.i18n.enforce_available_locales = true
+    config.i18n.default_locale = :en
+
+    # Protects against typical web attacks
     config.middleware.use Rack::Protection
 
     # Adds a X-Revision header with the current commit SHA
