331658969581e7552eafdcd347a622e88a33b6fe
Philippe Dionne <dionne.phil@gmail.com>
2013-12-13 13:30:54 -0500

    Add ScheduleValidator specs

--- app/models/validators/schedule_validator.rb
+++ app/models/validators/schedule_validator.rb
@@ -3,7 +3,7 @@ class ScheduleValidator < ActiveModel::Validator
   # @param record [Rent]
   def validate(record)
     validate_end_time_is_after_start_time(record) if record.end_time.present?
-    validate_start_time_is_after_today(record) if record.start_time.present? && !record.persisted?
+    validate_start_time_not_in_past(record) if record.start_time.present? && !record.persisted?
   end
 
 
@@ -15,7 +15,7 @@ class ScheduleValidator < ActiveModel::Validator
     end
   end
 
-  def validate_start_time_is_after_today(record)
+  def validate_start_time_not_in_past(record)
     unless record.start_time > Proc.new { Date.yesterday.at_end_of_day }.call
       record.errors.add(:start_time, "start time must be after today")
     end
--- spec/models/validators/schedule_validator_spec.rb
+++ spec/models/validators/schedule_validator_spec.rb
@@ -2,9 +2,21 @@ require 'spec_helper'
 
 describe ScheduleValidator do
 
-  describe :validate_end_date_is_after_start_date do
+  describe :validate_end_time_is_after_start_time do
+    let(:schedule) { build(:schedule, start_time: Date.today, end_time: Date.yesterday) }
+
+    it "validates end time is after start time" do
+      schedule.valid?
+      expect(schedule).to have(1).error_on(:end_time)
+    end
   end
 
-  describe :validate_start_date_is_after_today do
+  describe :validate_start_time_not_in_past do
+    let(:schedule) { build(:schedule, start_time: Date.yesterday) }
+
+    it "validates start time not in the past" do
+      schedule.valid?
+      expect(schedule).to have(1).error_on(:start_time)
+    end
   end
 end
