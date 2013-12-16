ef65ea10568d74d63e07f1c7d6a8c442210c3182
Philippe Dionne <dionne.phil@gmail.com>
2013-12-16 14:26:30 -0500

    Fix Vehicle year format validation

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -2,7 +2,7 @@ class Vehicle < ActiveRecord::Base
   include Authority::Abilities
 
   validates :user, :license_plate, presence: true
-  validates :year, format: { with: /\A\d{4}\z/ }
+  validates :year, format: { with: /\A\d{4}\z/, allow_blank: true }
   validates :license_plate, license_plate: true
 
   belongs_to :user
